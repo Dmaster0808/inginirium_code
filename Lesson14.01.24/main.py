5e24285c14f7648047385eaee4e3264f2b83c761
loliland11231 <125196041+loliland11231@users.noreply.github.com>
2024-01-14 17:48:28 +0400

    изучали кубики

---
+++ Lesson14.01.24/main.py
@@ -0,0 +1,30 @@
+import pygame
+
+pygame.init()
+win = pygame.display.set_mode((600, 400))
+
+
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+                exit()
+    color = (0, 0, 255)
+
+
+
+    pygame.draw.rect(win, (255, 255, 255), (0, 0, 100, 100), 50)
+    pygame.draw.rect(win, (255, 255, 255), (0, 0, 100, 100), 50)
+
+
+
+
+
+
+
+
+
+    pygame.display.update()
+
+
